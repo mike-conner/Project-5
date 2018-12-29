89013a1b29b5ce6817f1894d96bbedeb5a3ce9e0
Mike Conner <connerland@yahoo.com>
2018-12-29 11:17:21 -0500

    populate data

---
+++ Project-5/Model/RandomVisitorData.swift
@@ -0,0 +1,19 @@
+//
+//  RandomVisitorData.swift
+//  Project-5
+//
+//  Created by Mike Conner on 12/27/18.
+//  Copyright © 2018 Mike Conner. All rights reserved.
+//
+
+import Foundation
+
+let randomSSN: [String] = ["111-11-1111", "222-22-2222", "333-33-3333", "444-44-4444", "555-55-5555"]
+let randomFirstName : [String] = ["Mike", "Bob", "Chuck", "Sally", "Karen"]
+let randomLastName : [String] = ["Smith", "Jackson", "Morris", "Thompson", "Lincoln"]
+let randomCompany : [String] = ["Dell", "Apple", "Logitech", "General Electric", "Samsung"]
+let randomStreetAddress : [String] = ["123 Ave NW", "234 Street NW", "345 Ave SE", "456 Street SW", "567 Ave SW"]
+let randomCity : [String] = ["Taylorsville", "Cancove", "Rennick", "Ackinsville", "Kinstown"]
+let randomState : [String] = ["ND", "SD", "MN", "OH", "KY"]
+let randomZipCode : [String] = ["12345", "23456", "34567", "45678", "56789"]
+
