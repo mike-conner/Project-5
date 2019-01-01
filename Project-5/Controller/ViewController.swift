11e355384f6e0568d76972d48184754eb87e2fb0
Mike Conner <connerland@yahoo.com>
2018-12-31 19:11:02 -0500

    Working on State Validation

--- Project-5/Controller/ViewController.swift
+++ Project-5/Controller/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     var entrantSubTypeIndex: Int = 0
     let projectNumbers = ["1001", "1002", "1003", "2001", "2002"]
     let companyNames = ["Acme", "Orkin", "Fedex", "NW Electrical"]
+    let states: [String] = ["Alabama", "AL", "Alaska", "AK", "Arizona", "AZ", "Arkansas", "AR", "California", "CA", "Colorado", "CO", "Connecticut", "CT", "Delaware", "DE", "Florida", "FL", "Georgia", "GA", "Hawaii", "HI", "Idaho", "ID", "Illinois", "IL", "Indiana", "IN", "Iowa", "IA", "Kansas", "KS", "Kentucky", "KY", "Louisiana", "LA", "Maine", "ME", "Maryland", "MD", "Massachusetts", "MA", "Michigan", "MI", "Minnesota", "MN", "Mississippi", "MS", "Missouri", "MO", "Montana", "MT", "Nebraska", "NE", "Nevada", "NV", "New Hampshire", "NH", "New Jersey", "NJ", "New Mexico", "NM", "New York", "NY", "North Carolina", "NC", "North Dakota", "ND", "Ohio", "OH", "Oklahoma", "OK", "Oregon", "OR", "Pennsylvania", "PA", "Rhode Island", "RI", "South Carolina", "SC", "South Dakota", "SD", "Tennessee", "TN", "Texas", "TX", "Utah", "UT", "Vermont", "VT", "Virginia", "VA", "Washington", "WA", "West Virginia", "WV", "Wisconsin", "WI", "Wyoming", "WY"]
     
     private var datePicker: UIDatePicker?
     
@@ -169,6 +170,11 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             self.present(alert, animated: true, completion: nil)
             return
         }
+        
+        /*if !states.contains(stateTextField.text ?? "") {
+            errorTitle = "Not A Valid State"
+            errorMessage = "Please enter a valid state."
+        }*/
         
         entrantPass = Pass(visitor: newVisitor)
         
